8e1dda328e2ccf48295a7effe2d4baca9ad8424c
Sungho Yoo <sunghoster@gmail.com>
2013-10-28 20:26:36 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Worldsearch::Application.config.secret_key_base = '483f1406ead935d661f2dd82f258e29d463dbce886e4cbf33dc496ea59e6e5368b0b67f4ec538039368fec954a176be4eb560eb7d3eac4b1593be7973e78742e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Worldsearch::Application.config.session_store :cookie_store, key: '_worldsearch_session'
